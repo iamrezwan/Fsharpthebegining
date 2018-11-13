23c19f0e363e154a80fd5ece9c5d8a782d299cbf
Office <rezwan@live.co.uk>
2018-11-13 15:16:25 +0600

    1st Commit

---
+++ HandsOnFsharp/Program.fs
@@ -0,0 +1,10 @@
+﻿open System
+
+[<EntryPoint>]
+let main argv =   
+        printfn "Hello what is your name"         
+        let name = Console.ReadLine() 
+        printfn "You said your name is %s" name
+        Console.ReadLine() |> ignore
+        0
+   
