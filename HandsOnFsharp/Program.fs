f4eff790b7e076e7ae4b7d5f583a19f7044dbfd0
Office <rezwan@live.co.uk>
2018-11-14 09:40:34 +0600

    2nd

--- HandsOnFsharp/Program.fs
+++ HandsOnFsharp/Program.fs
@@ -1,10 +1,13 @@
 ﻿open System
-
 [<EntryPoint>]
 let main argv =   
-        printfn "Hello what is your name"         
-        let name = Console.ReadLine() 
-        printfn "You said your name is %s" name
-        Console.ReadLine() |> ignore
+        let mutable myValue = 100
+        printfn "Value of myValue  is %i" myValue
+        myValue <- 200
+        printfn "Value of myValue  is %i" myValue
+        let myRefValue = ref 10
+        myRefValue := 20
+        printfn "Value of myValue  is %i" ! myRefValue
+        Console.ReadLine() |> ignore       
         0
-   
+  
